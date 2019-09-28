1d02bf7de1985150a2b98db2931f38dad0a72bc2
tdvance <deplorable.mountaineer@gmail.com>
2019-09-28 09:55:34 -0400

    Missile hits can destroy enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        ProcessHit(other.gameObject.GetComponent<DamageDealer>());
+    }
+
+    private void ProcessHit(DamageDealer damageDealer) {
+        if (damageDealer) {
+            health -= damageDealer.GetDamage();
+            damageDealer.Hit();
+            if (health <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
