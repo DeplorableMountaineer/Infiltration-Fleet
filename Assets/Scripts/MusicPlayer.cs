8368be5b22bf346fcb44a238ffc48303ea56bad9
tdvance <deplorable.mountaineer@gmail.com>
2019-10-04 13:51:53 -0400

    Added music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[] playList;
+
+    private double nextClipTime = 0;
+    private AudioSource[] audioSources;
+    private int currentAudioSource = 0;
+    private int currentClip = 0;
+    private double clipDuration = 0.5;
+
+    void Awake() {
+        if (FindObjectsOfType<MusicPlayer>().Length > 1) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+            audioSources = GetComponents<AudioSource>();
+        }
+    }
+
+    private IEnumerator Start() {
+        float delay;
+        while (true) {
+            delay = 0;
+            while (delay < 1) {
+                delay += PlayNextClip();
+            }
+            yield return new WaitForSeconds(delay - 1);
+        }
+    }
+
+    private float PlayNextClip() {
+        if (nextClipTime <= 0) {
+            nextClipTime = AudioSettings.dspTime;
+        }
+        nextClipTime += clipDuration;
+        clipDuration = (double)(playList[currentClip].samples) / playList[currentClip].frequency;
+        audioSources[currentAudioSource].clip = playList[currentClip];
+        audioSources[currentAudioSource].PlayScheduled(nextClipTime);
+        currentClip = (currentClip + 1) % playList.Length;
+        currentAudioSource = (currentAudioSource + 1) % audioSources.Length;
+        return (float)(nextClipTime - AudioSettings.dspTime + clipDuration);
+    }
+}
+
